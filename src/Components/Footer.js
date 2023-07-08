e71eb10afda34d5684eaf77100d20e7016d3c1ba
Camsito <al.grumiz@gmail.com>
2023-07-08 04:00:35 +0200

    Version 1.0.2

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ export function Footer() {
       <div class="container top-footer">
         <div class="flex-center">
           <a class="fa fa-facebook fa-4x icon-3d" href="https://web.facebook.com/profile.php?id=100094334372935" target="_blank" rel="noreferrer"></a>
-          <a class="fa fa-instagram fa-4x icon-3d" href="https://www.instagram.com/dinotechno/" target="_blank" rel="noreferrer"></a>
+          <a class="fa fa-instagram fa-4x icon-3d" href="https://www.instagram.com/longdino.oficial/" target="_blank" rel="noreferrer"></a>
           <a class="fa fa-whatsapp fa-4x icon-3d" href="https://api.whatsapp.com/send?phone=+56936681029&text=Hola, Quisiera hacer unas consultas." target="_blank" rel="noreferrer"></a>
         </div>
       </div>
