e6b721c6d4618f65bb0fd9c99d2d9485b3f620b3
Camsito <al.grumiz@gmail.com>
2023-07-22 21:28:42 -0400

    Version 1.0.8

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //-------------Componentes-------------//
 import Navbar from './Components/Navbar';
-// import Comp1 from './Components/Componente1';
-// import Comp2 from './Components/Componente2';
-// import { Footer } from './Components/Footer';
-// import Wsp from './Components/WhatsappPNG';
-// import Copyri from './Components/Copyri';
+import Comp1 from './Components/Componente1';
+import Comp2 from './Components/Componente2';
+import { Footer } from './Components/Footer';
+import Wsp from './Components/WhatsappPNG';
+import Copyri from './Components/Copyri';
 //-------------StyleSheet-------------//
 import "./style.css";
-import "./Botons.css"
-import Contacts from './Components/Contact';
+import "./Botons.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Navbar/>
-    <Contacts/>
+    <Comp1/>
+    <Comp2/>
+    <Footer/>
+    <Copyri/>
+    <Wsp/>
   </>
 );
