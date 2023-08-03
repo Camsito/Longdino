a6c166f394068f532402810096817af20b4c0dd1
Camsito <al.grumiz@gmail.com>
2023-08-04 00:10:54 +0200

    Version 1.1.9

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,16 +1,79 @@
+import '../Stile.css';
+
 export function Footer() {
   /* eslint-disable */
   return (
-    <footer>
-      <div className='line'/>
-      <div class="container top-footer">
-        <div class="flex-center">
-          <a class="fa fa-facebook fa-4x icon-3d" href="https://web.facebook.com/profile.php?id=100094334372935" target="_blank" rel="noreferrer"></a>
-          <a class="fa fa-instagram fa-4x icon-3d" href="https://www.instagram.com/longdino.oficial/" target="_blank" rel="noreferrer"></a>
-          <a class="fa fa-whatsapp fa-4x icon-3d" href="https://api.whatsapp.com/send?phone=+56936681029&text=Hola, Quisiera hacer unas consultas." target="_blank" rel="noreferrer"></a>
-        </div>
-      </div>
-      <div className='line'/>
-    </footer>
+    // <footer>
+    //   <div className='line'/>
+    //   <div class="container top-footer">
+    //     <div class="flex-center">
+    //       <a class="fa fa-facebook fa-4x icon-3d" href="https://web.facebook.com/profile.php?id=100094334372935" target="_blank" rel="noreferrer"></a>
+    //       <a class="fa fa-instagram fa-4x icon-3d" href="https://www.instagram.com/longdino.oficial/" target="_blank" rel="noreferrer"></a>
+    //       <a class="fa fa-whatsapp fa-4x icon-3d" href="https://api.whatsapp.com/send?phone=+56936681029&text=Hola, Quisiera hacer unas consultas." target="_blank" rel="noreferrer"></a>
+    //     </div>
+    //   </div>
+    //   <div className='line'/>
+    // </footer>
+
+
+
+<div class="footer">
+  <div class="containearr">
+  <div class="col">
+    <h1>PYME</h1>
+    <ul>
+      <li>Sobre Nosotros</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Products</h1>
+    <ul>
+      <li>About</li>
+      <li>Mission</li>
+      <li>Services</li>
+      <li>Social</li>
+      <li>Get in touch</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Accounts</h1>
+    <ul>
+      <li>About</li>
+      <li>Mission</li>
+      <li>Services</li>
+      <li>Social</li>
+      <li>Get in touch</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Resources</h1>
+    <ul>
+      <li>Webmail</li>
+      <li>Redeem code</li>
+      <li>WHOIS lookup</li>
+      <li>Site map</li>
+      <li>Web templates</li>
+      <li>Email templates</li>
+    </ul>
+  </div>
+  <div class="col">
+    <h1>Support</h1>
+    <ul>
+      <li>Contact us</li>
+      <li>Web chat</li>
+      <li>Open ticket</li>
+    </ul>
+  </div>
+  <div class="col social">
+    <h1>Social</h1>
+    <ul>
+      <li><img src="https://svgshare.com/i/5fq.svg" width="32"/></li>
+      <li><img src="https://svgshare.com/i/5eA.svg" width="32"/></li>
+      <li><img src="https://svgshare.com/i/5f_.svg" width="32"/></li>
+    </ul>
+  </div>
+<div class="clearfix"></div>
+</div>
+</div>
   );
 }
