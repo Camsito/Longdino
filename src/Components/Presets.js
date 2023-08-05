1688ddece838292d0e551da8a982d93d1b1d0960
Camsito <al.grumiz@gmail.com>
2023-08-05 18:40:02 -0400

    Version 1.2.2

--- src/Components/Presets.js
+++ src/Components/Presets.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Footer } from "./Footer";
 import Copyri from "../Components/Copyri";
 import Greeting from "./WhatsappPNG";
-import { Pc1 } from "../assets/Productos/Pc1";
+import { Pc1, Pc2, Pc3 } from "../assets/Productos/Pc1";
 import "../Catalog.css";
 
 function Presets() {
@@ -21,14 +21,17 @@ function Presets() {
                   </div>
                   <div class="card__body">
                     <div class="card__title">Características</div>
-                    <div class="card__text">-PRIME H510M-E LGA 1200<br/>
-                                            -INTEL CORE I5-10400F LGA 1200<br/>
-                                            -RAM FURY BEAST 8GB DDR4 3200<br/>
-                                            -500GB SSD 3500MB/s<br/>
-                                            -Grafica a elegir
-                                            (precio puede variar)</div>
+                    <div class="shopline"></div>
+                    <div class="card__text">-Placa: MSI A320M-A PRO MAX<br/>
+                                            -Procesador: Ryzen 7 5700 G<br/>
+                                            -Graficos: Integrada<br/>
+                                            -Ram: 16 GB 2X8 3200 MHZ<br/>
+                                            -Almacenamiento: SSD 1TB<br/>
+                                            -Fuente: 500w 80 PLUS<br/>
+                                            -Gabinete a eleccion
+                                            </div>
                     <div class="card__bottom">
-                      <div class="card__cost">$750.000</div>
+                      <div class="card__cost">$549.000</div>
                       <div class="card__button button">
                         <a
                           href="https://api.whatsapp.com/send?phone=+56936681029&text=Hola
@@ -47,23 +50,31 @@ function Presets() {
               <div class="catalog__col">
                 <div class="catalog__card card">
                   <div class="card__image">
-                    <img src="https://picsum.photos/1920/1081" alt="" />
+                    <img src={Pc2} alt="" />
                   </div>
                   <div class="card__body">
-                    <div class="card__title">Computador</div>
-                    <div class="card__text">Descripcion Computador</div>
+                    <div class="card__title">Características</div>
+                    <div class="shopline"></div>
+                    <div class="card__text">-Placa: Gigabyte A520M DS3H Socket AM4<br/>
+                                            -Procesador: Ryzen 5 5500 Socket AM4<br/>
+                                            -Grafica: Zotac GAMING GeForce RTX 3050<br/>
+                                            -Ram: 16 GB 2X8 3200 MHZ<br/>
+                                            -Almacenamiento: 500GB SSD 3500MB/s<br/>
+                                            -Fuente: 500w 80 PLUS<br/>
+                                            -Gabinete a eleccion
+                                            </div>
                     <div class="card__bottom">
-                      <div class="card__cost">$750.000</div>
+                      <div class="card__cost">$747.000</div>
                       <div class="card__button button">
                         <a
                           href="https://api.whatsapp.com/send?phone=+56936681029&text=Hola
                         %2C%20quiero%20comprar%20un%20PC%20con%20las%20siguientes%20característica
-                        %3A%0A%0A-Característica
-                        %201%0A-Característica
-                        %202%0A-Característica
-                        %203%0A-Característica
-                        %204%0A-Característica
-                        %205%0A-Característica
+                        %3A%0A%0A-Placa: Gigabyte A520M DS3H Socket AM4
+                        %201%0A-Procesador: Ryzen 5 5500 Socket AM4
+                        %202%0A-Grafica: Zotac GAMING GeForce RTX 3050
+                        %203%0A-Ram: 16 GB 2X8 3200 MHZ
+                        %204%0A-500GB SSD 3500MB/s
+                        %205%0A-Gabinete a eleccion
                         %206">Solicitar</a>
                       </div>
                     </div>
@@ -74,13 +85,20 @@ function Presets() {
               <div class="catalog__col">
                 <div class="catalog__card card">
                   <div class="card__image">
-                    <img src="https://picsum.photos/1920/1082" alt="" />
+                    <img src={Pc3} alt="" />
                   </div>
                   <div class="card__body">
-                    <div class="card__title">Computador</div>
-                    <div class="card__text">Descripcion Computador</div>
+                    <div class="card__title">Característica</div>
+                    <div class="shopline"></div>
+                    <div class="card__text">-Placa: ASUS PRIME A520M-A II/CSM AM4<br/>
+                                            -Procesador: Ryzen 5 5500<br/>
+                                            -Grafica: GTX 1660 SUPER<br/>
+                                            -Ram: Kingston Fury Beast 8GB x2<br/>
+                                            -Fuente: EVGA 600W 80 PLUS<br/>
+                                            -Almacenamiento: 500GB SSD 3500MB/s<br/>
+                                            -Gabinete a eleccion</div>
                     <div class="card__bottom">
-                      <div class="card__cost">$750.000</div>
+                      <div class="card__cost">$700.000</div>
                       <div class="card__button button">
                         <a
                           href="https://api.whatsapp.com/send?phone=+56936681029&text=Hola
@@ -105,6 +123,7 @@ function Presets() {
                   </div>
                   <div class="card__body">
                     <div class="card__title">Computador</div>
+                    <div class="shopline"></div>
                     <div class="card__text">Descripcion Computador</div>
                     <div class="card__bottom">
                       <div class="card__cost">$750.000</div>
@@ -132,6 +151,7 @@ function Presets() {
                   </div>
                   <div class="card__body">
                     <div class="card__title">Computador</div>
+                    <div class="shopline"></div>
                     <div class="card__text">Descripcion Computador</div>
                     <div class="card__bottom">
                       <div class="card__cost">$750.000</div>
@@ -159,6 +179,7 @@ function Presets() {
                   </div>
                   <div class="card__body">
                     <div class="card__title">Computador</div>
+                    <div class="shopline"></div>
                     <div class="card__text">Descripcion Computador</div>
                     <div class="card__bottom">
                       <div class="card__cost">$750.000</div>
--- src/assets/Productos/Pc1.js
+++ src/assets/Productos/Pc1.js
@@ -1,8 +1,8 @@
 import Pc1 from './Pc1-i5-10400F-Prime-H510M-E-LGA1200.png';
-// import Pc2 from './';
-// import Pc3 from './';
+import Pc2 from './Pc2-R5-5500-Gigabyte-A520M-DS3H-AM4.png';
+import Pc3 from './Pc3-R5-5500-AsusPrime-A520MA-ll-AM4.png';
 // import Pc4 from './';
 // import Pc5 from './';
 // import Pc6 from './';
 
-export {Pc1};
+export {Pc1, Pc2,Pc3};
